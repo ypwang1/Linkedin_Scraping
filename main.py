27bcf1218dcd1f56a85c7274f2e006df65d8da05
Yupeng Wang <yupengori@gmail.com>
2025-06-23 18:13:52 +0200

    save job listings

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import time
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 URL = "https://www.linkedin.com/jobs/search/?currentJobId=4251877298&f_AL=true&keywords=devops%20engineer&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true"
 EMAIL = "yupeng.100daysofcode@gmail.com"
-PASSWORD = "Wyp5618256@"
+PASSWORD = os.getenv("PASSWORD")
 
 chrome_option = webdriver.ChromeOptions()
 chrome_option.add_experimental_option("detach", True)
@@ -28,16 +31,21 @@ sign_in = driver.find_element(By.XPATH, value='//*[@id="base-sign-in-modal"]/div
 sign_in.click()
 time.sleep(3)
 
-save = driver.find_element(By.XPATH, value='//*[@id="jobs-apply-button-id"]')
-save.click()
-time.sleep(1)
-
-mobel = driver.find_element(By.XPATH, value='//*[@id="single-line-text-form-component-formElement-urn-li-jobs-applyformcommon-easyApplyFormElement-4251877298-21438008524-phoneNumber-nationalNumber"]')
-mobel.send_keys("0635314494")
-
-next =driver.find_element(By.XPATH, value='//*[@id="ember379"]')
-next.click()
-time.sleep(1)
-
-next_2 =driver.find_element(By.XPATH, value='//*[@id="ember379"]')
-next_2.click()
+index = 0
+while True:
+    jobs = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
+    if index >= len(jobs):
+        print("No more jobs found.")
+    for job in jobs:
+        driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", job)
+        time.sleep(1.5)
+        job.click()
+        time.sleep(2)
+        try:
+            save = driver.find_element(By.CSS_SELECTOR, value='.jobs-save-button__text')
+            if save.text != "Saved":
+                save.click()
+                print("job saved")
+        except NoSuchElementException:
+            print("No save button, skipped")
+            continue
